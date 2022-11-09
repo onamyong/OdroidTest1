9a987291afa53520dedaebb89d0f3a2d9626e2b9
onamyong <onamyong.blog@gmail.com>
2022-11-09 20:13:35 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+const string prjName = "OdroidTest1";
+
+int main() {
+    cout << prjName << "에 오신 것을 환영합니다!" << endl;
+
+    vector<string> lines;
+
+    getchar();
+    return 0;
+}
