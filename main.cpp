0d83ede0f976cfabb17181b7a862cd9039568655
onamyong <onamyong.blog@gmail.com>
2022-11-11 22:22:08 +0900

    Note

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
-#include "MySTLUtil.h"
+//#include "MySTLUtil.h"
+#include "wiringPi.h"
 
 using namespace std;
 const string prjName = "OdroidTest1";
@@ -10,6 +11,14 @@ int main() {
     cout << prjName << "에 오신 것을 환영합니다!" << endl;
 
     vector<string> lines;
+
+    wiringPiSetup();
+
+    int i = 0;
+    while (true) {
+        cout << i++ << endl;
+            delay(1000);
+    }
 
     getchar();
     return 0;
